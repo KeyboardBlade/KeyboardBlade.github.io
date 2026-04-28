9c9f08ae3a2f5f29c5a17fd22c8179688906f1d4
Daniel Ashby <dashby.dev@gmail.com>
2026-04-28 19:26:16 -0400

    updates to site

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/KeyboardBlade.github.io/', // Must start and end with a forward slash
+  // base: '/KeyboardBlade.github.io/', // Must start and end with a forward slash
   plugins: [react()],
 })
