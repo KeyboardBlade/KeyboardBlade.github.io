017ad1fa0e4de17509b39e536f580da62558c75d
Daniel Ashby <dashby.dev@gmail.com>
2026-05-02 11:07:05 -0400

    Add tailwind and shadcn configurations.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,13 +22,13 @@ function App() {
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button
-          type="button"
-          className="counter"
+        <Button
+          variant={"destructive"}
+          className="bg-black"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
-        </button>
+          count is {count}
+        </Button>
       </section>
 
       <div className="ticks"></div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/KeyboardBlade.github.io/', // Must start and end with a forward slash
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
